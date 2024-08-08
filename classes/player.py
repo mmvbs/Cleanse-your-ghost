2a19e13157164d43f928c1059d092c5d1395fea5
ANTONIO CAUE OLIVEIRA MORAIS <acompcelular@gmail.com>
2024-08-08 00:23:52 -0300

    Cooldown e pequenas mudancas

--- classes/player.py
+++ classes/player.py
@@ -25,7 +25,7 @@ class Player:
         self.gravity = 1
         self.tempo_pulo = self.tempopulo
 
-        self.tamanho_imagem = (384, 384)
+        self.tamanho_imagem = (384, 384) #Tamanho do Personagem
 
         self.correndo = [
             carregar_imagem(os.path.join('texturas', 'correr1.png'), self.tamanho_imagem),
@@ -36,13 +36,23 @@ class Player:
         self.atacando = [
             carregar_imagem(os.path.join('texturas', 'ataque1.png'), self.tamanho_imagem),
             carregar_imagem(os.path.join('texturas', 'ataque2.png'), self.tamanho_imagem),
-            carregar_imagem(os.path.join('texturas', 'ataque3.png'), self.tamanho_imagem),
-            carregar_imagem(os.path.join('texturas', 'ataque4.png'), self.tamanho_imagem),
+            #carregar_imagem(os.path.join('texturas', 'ataque3.png'), self.tamanho_imagem),
+            #carregar_imagem(os.path.join('texturas', 'ataque4.png'), self.tamanho_imagem),
         ]
         self.step_index = 0
         self.ataque_index = 0
         self.image = self.atacando[0]
         self.image = self.correndo[0]
+
+        #Cooldown
+        self.cooldown_ataque = 750
+        self.ultimo_ataque = 0
+        # Tela de Cooldown
+        self.barra_cor = (255, 0, 0)  
+        self.barra_fundo_cor = (0, 0, 0)  
+        self.barra_altura = 12  
+        self.barra_largura = self.tamanho_imagem[0]/5#Tamanho da barra de cooldown
+        
         
     def load_images(self):
         base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'texturas'))
@@ -51,30 +61,30 @@ class Player:
         player_down_path = os.path.join(base_path, "ocultiedown.png")
         self.player_down_image = carregar_imagem(player_down_path, self.tamanho_imagem)
 
-    def draw(self):
-        self.tela.blit(self.image, (self.x_player, self.y_player))
-
     def ataque(self):
+        tempo_atual = pygame.time.get_ticks()
+        if tempo_atual - self.ultimo_ataque >= self.cooldown_ataque:
             self.image = self.atacando[self.ataque_index]
             self.ataque_index += 1
             if self.ataque_index >= len(self.atacando):
                 self.ataque_index = 0
-
+            self.ultimo_ataque = tempo_atual
+                
     def run(self):
         # Atualiza o frame da animação de corrida
         self.image = self.correndo[self.step_index // 5]
         self.step_index = (self.step_index + 1) % (len(self.correndo) * 5)  # Mantém o índice dentro dos limites
         
         #Atualiza Fisica do Pulo
-        self.atualizar()
+        self.atualiza_pulo()
 
     def jump(self):
         #Faz o personagem pular
         if not self.pulando:
-            self.pulando =True
+            self.pulando = True
             self.tempo_pulo = self.tempopulo
     
-    def atualizar(self):
+    def atualiza_pulo(self):
         # Atualiza a física do pulo
         if self.pulando:
             self.y_player += self.velocidade_pulo
@@ -87,3 +97,22 @@ class Player:
                 self.pulando = False
                 self.velocidade_pulo = self.AlturaPulo
                 self.tempo_pulo = self.tempopulo
+    
+
+    def draw(self):
+        self.tela.blit(self.image, (self.x_player, self.y_player))#Desenha personagem
+    
+        self.draw_cooldown_bar()#Desenha Cooldown
+    
+    def draw_cooldown_bar(self):
+        tempo_atual = pygame.time.get_ticks()
+        tempo_restante = max(0, self.cooldown_ataque - (tempo_atual - self.ultimo_ataque))
+
+        # Calcula a largura da barra
+        barra_largura = ((tempo_restante / self.cooldown_ataque) * self.barra_largura)
+
+        # Desenha a barra de fundo
+        pygame.draw.rect(self.tela, self.barra_fundo_cor, (self.x_player*5, self.y_player - (-200), self.barra_largura, self.barra_altura))
+
+        # Desenha a barra de cooldown
+        pygame.draw.rect(self.tela, self.barra_cor, (self.x_player*5, self.y_player - (-200), barra_largura, self.barra_altura))
