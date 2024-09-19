6e7ad74df81023b78165424a0ff1e0948b98f60e
mM-b <gtamsue@gmail.com>
2024-09-19 13:16:19 -0300

    att em entidade

--- CYG.py
+++ CYG.py
@@ -9,39 +9,31 @@ from classes.fantasma import Fantasma
 from classes.obstaculos import Obstaculo
 from classes.cenario import Mundo, Nuvem
 
-
-pygame.init() 
+pygame.init()
 
 # Configura a tela
 largura = 1280
 altura = 720
-
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Cleanse your Ghost")
 
-# Instancias
+# Instâncias
 jogador = pl.Player(largura, altura, tela)
-jogador.load_images()
 mundo = Mundo(largura, altura, tela)
-mundo.load_images("bg.png", (1280,500))
 nuvem = Nuvem(largura, altura, tela)
-nuvem.load_images("nuvem.png", (630,100))
 tumulo = Obstaculo(largura, altura, tela, 900, 480, 12, "stone-1.png", (100, 140))
 moita = Obstaculo(largura, altura, tela, 400, 500, 12, "bush-large.png", (152, 130))
 fantasma = fa.Fantasma(largura, altura, tela)
 
-#fantasma.load_images()
 def iniciar():
     global jogador, tumulo, fantasma, mundo, nuvem, moita
     jogador = pl.Player(largura, altura, tela)
-    jogador.load_images()
     mundo = Mundo(largura, altura, tela)
-    mundo.load_images("bg.png", (1280,500))
     nuvem = Nuvem(largura, altura, tela)
-    nuvem.load_images("nuvem.png", (630,100))
     tumulo = Obstaculo(largura, altura, tela, 900, 480, 12, "stone-1.png", (100, 140))
     moita = Obstaculo(largura, altura, tela, 400, 500, 12, "bush-large.png", (152, 130))
     fantasma = fa.Fantasma(largura, altura, tela)
+
 # Loop principal do jogo
 running = True
 clock = pygame.time.Clock()
@@ -51,13 +43,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
     tela.fill("black")
+
+    # Atualização dos objetos do jogo
     mundo.update()
     tumulo.update()
     moita.update()
     nuvem.update()
     fantasma.update()
-        # Desenha
+
+    # Desenho dos objetos na tela
     mundo.draw()
     fantasma.draw()
     tumulo.draw()
@@ -65,21 +61,24 @@ while running:
     nuvem.draw()
     jogador.draw()
     jogador.run()
-    
+
+    # Verifica colisões
     if jogador.checar_colisaoataque(moita):
         moita.x = 1290
-        jogador.ataquerect = pygame.Rect(jogador.x +150, 700, 200, 150)
+        jogador.ataquerect = pygame.Rect(jogador.x + 150, 700, 200, 150)
         fantasma.velocidade = 3
+
     if jogador.checar_colisao(moita):
         fantasma.velocidade = 0.5
+
     if jogador.checar_colisao(fantasma):
-        tela.blit(pygame.font.Font.render(pygame.font.Font(None, 64), "Você capturou o fantasma", True, (0, 255, 0)), ((largura/2)-200, altura/2)) 
+        tela.blit(pygame.font.Font.render(pygame.font.Font(None, 64), "Você capturou o fantasma", True, (0, 255, 0)), ((largura / 2) - 200, altura / 2))
         mundo.velocidade = 0
         tumulo.velocidade = 0
         moita.velocidade = 0
         nuvem.velocidade = 0
         fantasma.velocidade = 0
-        tela.blit(pygame.font.Font.render(pygame.font.Font(None, 64), "Precione r para re-iniciar", True, (255, 0, 0)), ((largura/2)-200, altura/4))
+        tela.blit(pygame.font.Font.render(pygame.font.Font(None, 64), "Pressione R para reiniciar", True, (255, 0, 0)), ((largura / 2) - 200, altura / 4))
         execucacao = False
 
         if pygame.key.get_pressed()[pygame.K_r]:
@@ -98,12 +97,12 @@ while running:
         moita.velocidade = 0
         nuvem.velocidade = 0
         fantasma.velocidade = 0
-        tela.blit(pygame.font.Font.render(pygame.font.Font(None, 64), "Precione r para re-iniciar", True, (255, 0, 0)), ((largura/2)-300, altura/4))
+        tela.blit(pygame.font.Font.render(pygame.font.Font(None, 64), "Pressione R para reiniciar", True, (255, 0, 0)), ((largura / 2) - 300, altura / 4))
         execucacao = False
 
         if pygame.key.get_pressed()[pygame.K_r]:
-            tumulo.stone1_x = 900
-            moita.bushLarge_x = 400
+            tumulo.x = 900
+            moita.x = 400
             mundo.velocidade = 12
             tumulo.velocidade = 12
             moita.velocidade = 12
@@ -113,25 +112,21 @@ while running:
             execucacao = True
             iniciar()
     
-    #pygame.draw.rect(tela, (255, 0, 0), jogador.rect, 2)
-    #pygame.draw.rect(tela, (255, 0, 0), tumulo.rect, 2) 
-    #pygame.draw.rect(tela, (255, 0, 0), fantasma.rect, 2)
-    #pygame.draw.rect(tela, (255, 0, 0), moita.rect, 2)
-    #pygame.draw.rect(tela, (255, 0, 0), jogador.ataquerect, 2)
-    
-
+    # Ataque do jogador
     if pygame.key.get_pressed()[pygame.K_j]:
         jogador.ataque()
+        
 
+        
+
+    # Pulo do jogador
     if pygame.key.get_pressed()[pygame.K_SPACE]:
         jogador.jump()
 
-    
-    
     # Atualiza a tela
     pygame.display.update()
 
     # Controla a taxa de frames por segundo
-    clock.tick(60) 
+    clock.tick(60)
 
 pygame.quit()
--- classes/player.py
+++ classes/player.py
@@ -8,51 +8,49 @@ def carregar_imagem(path, tamanho):
 
 class Player(Entidade):
     def __init__(self, largura, altura, tela):
-        super().__init__(largura, altura, tela, 40, 235, 5, 1)
+        super().__init__(largura, altura, tela, 40, 235, 5, 1)  # Chama o construtor da superclasse
 
-        # P/Pulo
-        self.AlturaPulo = -25  # Tamanho do pulo
+        # Pulo
+        self.AlturaPulo = -25
         self.tempopulo = 0
         self.pulando = False
         self.velocidade_pulo = self.AlturaPulo
         self.gravity = 1.5
         self.tempo_pulo = self.tempopulo
 
+        # Retângulos
         self.tamanho_imagem = (384, 384)
-        self.rect = pygame.Rect(self.x +150, 470, 80, 150)
-        self.ataquerect = pygame.Rect(self.x +150, 700, 200, 150)
+        self.rect = pygame.Rect(self.x + 150, 470, 80, 150)
+        self.ataquerect = pygame.Rect(self.x + 150, 700, 200, 150)
 
-        self.correndo = [
-            carregar_imagem(os.path.join('texturas', 'correr1.png'), self.tamanho_imagem),
-            carregar_imagem(os.path.join('texturas', 'correr2.png'), self.tamanho_imagem),
-            carregar_imagem(os.path.join('texturas', 'correr3.png'), self.tamanho_imagem),
-            carregar_imagem(os.path.join('texturas', 'correr4.png'), self.tamanho_imagem),
-        ]
-        self.atacando = [
-            carregar_imagem(os.path.join('texturas', 'ataque1.png'), self.tamanho_imagem),
-            carregar_imagem(os.path.join('texturas', 'ataque2.png'), self.tamanho_imagem),
-            carregar_imagem(os.path.join('texturas', 'ataque3.png'), self.tamanho_imagem),
-            carregar_imagem(os.path.join('texturas', 'ataque4.png'), self.tamanho_imagem),
-        ]
-        self.step_index = 0
-        self.ataque_index = 0
-        self.image = self.correndo[0]
+        # Carrega as imagens de corrida e ataque
+        self.carregar_imagens_corrida_e_ataque()
 
-        # Cooldown e ataque
+        # Cooldown de ataque
         self.cooldown_ataque = 750
         self.ultimo_ataque = 0
         self.ataque_count = 0
         self.barra_cor = (255, 0, 0)
         self.barra_fundo_cor = (0, 0, 0)
         self.barra_altura = 12
-        self.barra_largura = self.tamanho_imagem[0] / 5  # Tamanho da barra de cooldown
+        self.barra_largura = self.tamanho_imagem[0] / 5
+        self.ataque_index = 0
 
-    def load_images(self):
-        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'texturas'))
-        sprite_path = os.path.join(base_path, "ocultie1.png")
-        self.player_image = carregar_imagem(sprite_path, self.tamanho_imagem)
-        player_down_path = os.path.join(base_path, "ocultiedown.png")
-        self.player_down_image = carregar_imagem(player_down_path, self.tamanho_imagem)
+    def carregar_imagens_corrida_e_ataque(self):
+        caminhos_corrida = [
+            os.path.join('texturas', 'correr1.png'),
+            os.path.join('texturas', 'correr2.png'),
+            os.path.join('texturas', 'correr3.png'),
+            os.path.join('texturas', 'correr4.png'),
+        ]
+        self.carregar_imagens(caminhos_corrida, self.tamanho_imagem)  
+
+        self.atacando = [
+            carregar_imagem(os.path.join('texturas', 'ataque1.png'), self.tamanho_imagem),
+            carregar_imagem(os.path.join('texturas', 'ataque2.png'), self.tamanho_imagem),
+            carregar_imagem(os.path.join('texturas', 'ataque3.png'), self.tamanho_imagem),
+            carregar_imagem(os.path.join('texturas', 'ataque4.png'), self.tamanho_imagem),
+        ]
 
     def ataque(self):
         tempo_atual = pygame.time.get_ticks()
@@ -71,30 +69,24 @@ class Player(Entidade):
                 self.ataquerect = pygame.Rect(self.x +150, 700, 200, 150)
                 self.ultimo_ataque = tempo_atual
                 self.ataque_count = 0
+        
 
     def run(self):
-        # Atualiza o frame da animação de corrida
-        self.image = self.correndo[self.step_index // 5]
-        self.step_index = (self.step_index + 1) % (len(self.correndo) * 5)  # Mantém o índice dentro dos limites
-
-        # Atualiza Física do Pulo
+        super().atualizar_animacao(5)  
         self.atualiza_pulo()
 
     def jump(self):
-        # Faz o personagem pular
         if not self.pulando:
             self.pulando = True
             self.tempo_pulo = self.tempopulo
 
     def atualiza_pulo(self):
-        # Atualiza a física do pulo
         if self.pulando:
             self.y += self.velocidade_pulo
             self.rect.y += self.velocidade_pulo
             self.tempo_pulo += 1
             self.velocidade_pulo += self.gravity
 
-            # Verifica se o pulo terminou (se o jogador voltou ao chão)
             if self.y >= 235:
                 self.y = 235
                 self.rect.y = 470
@@ -103,23 +95,15 @@ class Player(Entidade):
                 self.tempo_pulo = self.tempopulo
 
     def draw(self):
-        self.tela.blit(self.image, (self.x, self.y))  # Desenha personagem
-        self.draw_cooldown_bar()  # Desenha Cooldown
+        super().desenhar()  
+        self.draw_cooldown_bar()
 
     def draw_cooldown_bar(self):
         tempo_atual = pygame.time.get_ticks()
         tempo_restante = max(0, self.cooldown_ataque - (tempo_atual - self.ultimo_ataque))
-
-        # Calcula a largura da barra
         barra_largura = (tempo_restante / self.cooldown_ataque) * self.barra_largura
 
-        # Desenha a barra de fundo
         pygame.draw.rect(self.tela, self.barra_fundo_cor, (self.x * 5, self.y - (-200), self.barra_largura, self.barra_altura))
-
-        # Desenha a barra de cooldown
         pygame.draw.rect(self.tela, self.barra_cor, (self.x * 5, self.y - (-200), barra_largura, self.barra_altura))
-
-    def checar_colisao(self, objeto):
-        return self.rect.colliderect(objeto.rect)
     def checar_colisaoataque(self, objeto):
         return self.ataquerect.colliderect(objeto.rect)
